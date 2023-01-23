88a2b5ae16f9367255cb967492fedd2b7e96bf03
Aleksandar Garkov <55351778+agarkov8@users.noreply.github.com>
2023-01-23 12:00:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText item;
+    Button add;
+    ListView listView;
+    ArrayList<String> itemList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        item = findViewById(R.id.editText);
+        add = findViewById(R.id.button);
+        listView = findViewById(R.id.list);
+
+        //Check if there is any saved data to the list and write it to the listView
+        itemList = FileHelper.readData(this);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, itemList);
+        listView.setAdapter(arrayAdapter);
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemName = item.getText().toString();
+                itemList.add(itemName);
+                item.setText("");
+                FileHelper.writeData(itemList, getApplicationContext());
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Delete");
+                alert.setMessage("Do you want to delete this item from the list?");
+                alert.setCancelable(false);
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        itemList.remove(position);
+                        arrayAdapter.notifyDataSetChanged();
+                        FileHelper.writeData(itemList,getApplicationContext());
+
+                    }
+                });
+
+                AlertDialog alertDialog = alert.create();
+                alertDialog.show();
+            }
+        });
+    }
+}
